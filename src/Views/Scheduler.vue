9f30af565bc725607689003c84fbb5f83f2fb0c9
hello2sujeet <hello2sujeet@gmail.com>
2020-04-10 10:23:54 +0530

    Added attendees functionality for Scheduler

--- src/Views/Scheduler.vue
+++ src/Views/Scheduler.vue
@@ -5,7 +5,7 @@
         <span>Select a user to schedule an event: </span>
         <dropdownlist
             :data-items="userList"
-            :text-field="'first_name'"
+            :text-field="'name'"
             :data-item-key="'id'"
             :default-item="defaultItem"
             @change="onDropDownChange">
@@ -18,6 +18,7 @@
                         :height="500"
                         :timezone="'Etc/UTC'"
                         :event-template="eventTemplate"
+                        :resources="resources"
                         @edit="onEdit"
                         @add="onAdd"
                         @cancel="onCancel"
@@ -38,91 +39,36 @@
 // Open this import statement to consume any API.
 //import axios from "axios";
 
+import user from '../user'
+import attendees from '../attendees'
+import scheduleData from '../schedule'
+
 export default {
   name: 'Scheduler',
   data () {
       return {
-        //baseURL: 'https://demoapis.free.beeceptor.com/',
-        scheduleFlag: false,
+            //baseURL: 'https://demoapis.free.beeceptor.com/',
+            scheduleFlag: false,
 
-        // Dummy data for user drop-down.
-        userList: [
-        {
-            "id": "5e8c2900f8c528a0902c2d32",
-            "first_name": "Bhoodev",
-            "last_name": "Dubey",
-            "skills": "[html: 5, vue: 5, js: 5,angular:5,vue:5,jquery:5,react:5] "
-        },
-        {
-            "id": "5e8c2900af159fc87056fe10",
-            "first_name": "sunakshi",
-            "last_name": "chagti",
-            "skills": "[html: 5, vue: 5, js: 5,angular:5,vue:5,jquery:5,react:5] "
-        },
-        {
-            "id": "5e8c2900f68e91080f45ced8",
-            "first_name": "Danish",
-            "last_name": "Arora",
-            "skills": "[html: 5, vue: 5, js: 5,angular:5,vue:5,jquery:5,react:5] "
-        },
-        {
-            "id": "5e8c2900f20bd592d65c9511",
-            "first_name": "Rajesh",
-            "last_name": "Roshan",
-            "skills": "[html: 5, vue: 5, js: 5,angular:5,vue:5,jquery:5,react:5] "
-        },
-        {
-            "id": "5e8c2900a015a3591a357240",
-            "first_name": "sujit",
-            "last_name": "kumar",
-            "skills": "[html: 5, vue: 5, js: 5,angular:5,vue:5,jquery:5,react:5] "
-        },
-        {
-            "id": "5e8c2900f769b5a28004a4b6",
-            "first_name": "Ayush",
-            "last_name": "Bafna",
-            "skills": "[html: 5, vue: 5, js: 5,angular:5,vue:5,jquery:5,react:5] "
-        },
-        {
-            "id": "5e8c2900fc6f0607f5be0684",
-            "first_name": "shrasti",
-            "last_name": "gupta",
-            "skills": "[html: 5, vue: 5, js: 5,angular:5,vue:5,jquery:5,react:5] "
-        },
-        {
-            "id": "5e8c290015cb11fc78c83586",
-            "first_name": "Arjun",
-            "last_name": "Rawal",
-            "skills": "[html: 5, vue: 5, js: 5,angular:5,vue:5,jquery:5,react:5] "
-        }
-        ],
-        defaultItem: { 
-            first_name: 'Select User'
-        },
-        firstName: "",
-        eventTemplate: '<div class="movie-template"><p>#: kendo.toString(start, "hh:mm") # - #: kendo.toString(end, "hh:mm") #</p> <h3>#: title #</h3></div>' ,
-        allDayEvenTemplate: "allDayEvenTemplate",
-        dateHeaderTemplate: "dateHeaderTemplate",
-        groupHeaderTemplate: "groupHeaderTemplate",
-        majorTimeHeaderTemplate: "majorTimeHeaderTemplate",
-        minorTimeHeaderTemplate: "minorTimeHeaderTemplate",
-        date: new Date(),
+            // Getting data from "user.json" file for user drop-down.
+            userList:user,
 
-        // Dummy data for Scheduler.
-        localDataSource: [
-        {
-            "id": 1,
-            "start": "2020/4/9 06:00 AM",
-            "end": "2020/4/9 09:00 AM",
-            "title": "POC Event"
-        },
-        {
-            "id": 2,
-            "start": "2020/4/7 08:00 AM",
-            "end": "2020/4/7 09:00 AM",
-            "title": "Internal Meeting"
-        }
-        ]
+            // Getting data from "schedule.json" file for Scheduler.
+            localDataSource: scheduleData,
+
+            resources: [],
+
+            defaultItem: { 
+                name: 'Select User'
+            },
+            firstName: "",
+            eventTemplate: '<div class="movie-template"><p>#: kendo.toString(start, "hh:mm") # - #: kendo.toString(end, "hh:mm") #</p> <h3>#: title #</h3></div>' ,
+            allDayEvenTemplate: "allDayEvenTemplate",
+            dateHeaderTemplate: "dateHeaderTemplate",
+            groupHeaderTemplate: "groupHeaderTemplate",
+            majorTimeHeaderTemplate: "majorTimeHeaderTemplate",
+            minorTimeHeaderTemplate: "minorTimeHeaderTemplate",
+            date: new Date()
         }
     },
     methods: {
@@ -161,13 +107,13 @@ export default {
 
             // Open this commented code to call the API for persisting the Schedule data.
 
-            // let payload = [{title: ev.event.title,
-            //     description: ev.event.description,
-            //     start: ev.event.start,
-            //     end: ev.event.end}]
+            /*let payload = [{title: ev.event.title,
+                description: ev.event.description,
+                start: ev.event.start,
+                end: ev.event.end}]
 
-            // axios.post('https://demoapiss.free.beeceptor.com/addschedule', payload)
-            // .then(response => (this.localDataSource = response["data"]))
+            axios.post('https://demoapiss.free.beeceptor.com/addschedule', payload)
+            .then(response => (this.localDataSource = response["data"]))*/
 
         },
         remove: function(ev) {
@@ -176,12 +122,21 @@ export default {
     }
 
     // Open this commented code to fetch the user drop-down data from API.
-    // ,
-    // mounted () {
-    //     axios
-    //     .get(this.baseURL + 'getskills')
-    //     .then(response => (this.userList = response["data"]));
-    // }
+    ,
+    mounted () {
+        /*axios
+        .get(this.baseURL + 'getskills')
+        .then(response => (this.userList = response["data"]));*/
+        console.log(attendees);
+        this.resources = [
+            {
+                field: "attendees", // The field of the Scheduler event which contains the resource identifier.
+                title: "Attendees", // The label displayed in the Scheduler edit form for this resource.
+                dataSource: attendees,
+                multiple: true // Indicate that this is a multiple instance resource.
+            }
+        ]
+    }
 }
 </script>
 <style scoped>
